954b0f1ddce459183cfc60c67075832620656555
Alexander Kehaya <afkehaya@gmail.com>
2016-08-03 15:11:53 -0700

    added FAQ Privacy Terms And Licens

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,5 +2,17 @@ class LandingController < ApplicationController
   def index
     render :layout => "empty"
   end
+  def faq
+    render :layout => "empty"
+  end
+  def privacy
+    render :layout => "empty"
+  end
+  def terms
+    render :layout => "empty"
+  end
+  def license
+    render :layout => "empty"
+  end
 
 end
